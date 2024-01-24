23949cd8ac0766b91d4c2e88142730bd13598549
algasami <105358814+algasami@users.noreply.github.com>
2024-01-24 18:14:47 +0800

    added flake

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "Vulkan-Guide Flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils/4022d587cbbfd70fe950c1e2083a02621806a725";
+  };
+
+  outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
+  let
+
+    lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
+    version = builtins.substring 0 0 lastModifiedDate;
+
+  in flake-utils.lib.eachDefaultSystem (system:
+    let
+      name = "vk_guide_flake";
+      src = ./.;
+      pkgs = import nixpkgs { inherit system; };
+      pkg_buildInputs = with pkgs; [
+        glslang
+        vulkan-headers
+        vulkan-loader
+        vulkan-validation-layers
+      ];
+      pkg_nativeBuildInputs = with pkgs; [
+        gcc12
+        cmake
+        gnumake
+        coreutils
+      ];
+    in
+    {
+      devShells.default = with pkgs; pkgs.mkShell {
+        buildInputs = pkg_buildInputs ++ pkg_nativeBuildInputs;
+        # include all of them so that it can be easier to debug
+        VULKAN_SDK = "${pkgs.vulkan-validation-layers}/share/vulkan/explicit_layer.d";
+      };
+    }
+  );
+}
