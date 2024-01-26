f47e1e13314b4f5a554006f3fdb12ed025174f3d
algasami <105358814+algasami@users.noreply.github.com>
2024-01-26 10:10:39 +0800

    read key input

--- src/vk_engine.cpp
+++ src/vk_engine.cpp
@@ -10,8 +10,14 @@
 #include <chrono>
 #include <thread>
 
+constexpr bool bUseValidationLayers = true;
+
+VulkanEngine *loadedEngine = nullptr;
+
 void VulkanEngine::init() {
 
+  assert(loadedEngine == nullptr);
+  loadedEngine = this;
   // We initialize SDL and create a window with it.
   SDL_Init(SDL_INIT_VIDEO);
 
@@ -29,6 +35,9 @@ void VulkanEngine::cleanup() {
 
     SDL_DestroyWindow(_window);
   }
+
+  // clear pointer
+  loadedEngine = nullptr;
 }
 
 void VulkanEngine::draw() {
@@ -46,8 +55,32 @@ void VulkanEngine::run() {
       // close the window when user alt-f4s or clicks the X button
       if (e.type == SDL_QUIT)
         bQuit = true;
+      if (e.type == SDL_WINDOWEVENT) {
+        if (e.window.event == SDL_WINDOWEVENT_MINIMIZED) {
+          stopRendering = true;
+        } else if (e.window.event == SDL_WINDOWEVENT_RESTORED) {
+          stopRendering = false;
+        }
+      }
+      // handles keyboard events
+      else if (e.type == SDL_KEYDOWN) {
+        // this adds the constant attribute to our keysym (locally)
+        fmt::println("scan: {}", SDL_GetScancodeName(e.key.keysym.scancode));
+        fmt::println("sym: {}", SDL_GetKeyName(e.key.keysym.sym));
+      } else if (e.type == SDL_KEYUP) {
+        SDL_Keysym const &keysym = e.key.keysym;
+      }
     }
-
+    if (stopRendering) {
+      // halt and stand by
+      std::this_thread::sleep_for(std::chrono::milliseconds(100));
+      continue;
+    }
     draw();
   }
+}
+
+VulkanEngine &VulkanEngine::Get() {
+  assert(loadedEngine != nullptr);
+  return *loadedEngine;
 }
