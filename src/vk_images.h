193ca7a77203aa8c8fb44ef1694e3093a530d37d
algasami <105358814+algasami@users.noreply.github.com>
2024-01-28 14:27:07 +0800

    added transition image

--- src/vk_images.h
+++ src/vk_images.h
@@ -1,7 +1,10 @@
 
-#pragma once 
+#pragma once
+
+#include "vk_types.h"
 
 namespace vkutil {
 
-
+void transition_image(VkCommandBuffer cmd, VkImage image,
+                      VkImageLayout currentLayout, VkImageLayout newLayout);
 };
